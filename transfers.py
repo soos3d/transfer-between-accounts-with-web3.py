890f45bb828ba78f0db008c3ff7c98034bb38f97
Soos3D <99700157+soos3d@users.noreply.github.com>
2022-09-11 18:27:29 -0300

    Update transfers.py

--- transfers.py
+++ transfers.py
@@ -9,7 +9,7 @@ if web3.isConnected():
 else:
     print("Connection Failed")
 
-# initialize the addresses
+# initialize the addresses and private key
 sender = "SENDER_ADDRESS"
 receiver = "RECEIVER_ADDRESS"
 private_key = "YOUR_PRIVATE_KEY"
@@ -17,12 +17,16 @@ private_key = "YOUR_PRIVATE_KEY"
 # nonce
 nonce = web3.eth.getTransactionCount(sender)
 
+# retrieve the gas limit for this simulated transaction.
+gas_limit = web3.eth.estimate_gas(({"from":sender,"to":receiver}), "latest")
+#print("The gas limit is:", gas_limit)
+
 # build the transaction
 tx = {
     "nonce": nonce,
     "to": receiver,
     "value": web3.toWei(1, "ether"),  # measure unit of 1 full token
-    "gas": 21000,
+    "gas": gas_limit,
     "gasPrice": web3.toWei(10, "gwei"),
     "chainId": web3.eth.chain_id,
 }
